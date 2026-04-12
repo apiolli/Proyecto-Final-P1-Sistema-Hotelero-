3571c5ae41fc22ca480995ae86e5f08088557e70
Star_ <Star_@MSI>
2026-04-12 17:10:41 -0400

    Modificacion de la clase reserva, creación de la clase empleadoDAO y reservaDAO

--- src/main/java/dao/ReservaDAO.java
+++ src/main/java/dao/ReservaDAO.java
@@ -4,10 +4,39 @@
  */
 package dao;
 
+import java.sql.SQLException;
+import modelo.Gestionable;
+import modelo.Reserva;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
 /**
  *
  * @author DELL
  */
-public class ReservaDAO {
+public class ReservaDAO implements Gestionable <Reserva> {
+    
+    private Connection con;
+        public ReservaDAO (Connection con){
+            this.con=con;
+        }
+    
+    @Override
+    public int guardar(Reserva reserva) throws SQLException {
+       PreparedStatement ps = con.prepareStatement("INSERT INTO reservas"
+       + "(id_reserva,nombre_huesped,total_personas,num_habitacion,fecha_entrada,fecha_salida,"
+       + "fecha_reserva,dinero_abonado,estado) VALUES(?,?,?,?,?,?,?,?,?)");
+        ps.setString(1, reserva.getIdReserva());
+        ps.setString(2, reserva.getNombreHuesped());
+        ps.setInt(3, reserva.getTotalPersonas());
+        ps.setInt(4, reserva.getNumHabitacion());
+        ps.setLong(5, reserva.getFechaEntrada());
+        ps.setLong(6, reserva.getFechaSalida());
+        ps.setLong(7, reserva.getFechaReserva());
+        ps.setDouble(8, reserva.getDineroAbonado());
+        ps.setString(9, reserva.getEstado());
+       return ps.executeUpdate();
+    }
     
 }
